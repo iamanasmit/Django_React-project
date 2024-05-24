a2b771f6855fdd32167b9097d3fc016a106c45b0
Anasmit Mitra <anasmitmitra9@gmail.com>
2024-05-24 15:57:40 +0530

    adding the frontend and backend folders

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,8 @@
+import CircularProgress from "@material-ui/core/CircularProgress";
+export default function Loader() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <CircularProgress size={100} />
+    </div>
+  );
+}
